86e1018fdf8767dea5f285058874902f56176c3b
AmiQT <muhammad460485@gmail.com>
2025-08-13 22:47:52 +0800

    Trigger fresh deployment - fix asset loading

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+// Trigger fresh deployment - GitHub Actions is now working
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
